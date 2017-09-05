ec70395978ffe28fc19abbc6778e6d0d886d4041
Jared Jones <jkjones.dev@gmail.com>
2017-09-05 12:51:49 -0500

    Fixing calendar and adding WIP message to course list + Deploy

--- src/components/pages/collection/Courses.vue
+++ src/components/pages/collection/Courses.vue
@@ -1,9 +1,9 @@
 <template lang="html">
 <div class="page-container">
-    <div class="page-body">
-
+    <div class="page-body content">
+	<h3>We're working on it!</h3>
+	<p>Come back soon for a complete list of UT design related courses</p>
     </div>
-
 </div>
 </template>
 
